c9286a7781d8fa493ffe536fab72b1935bf5bf11
manerger-manu <144687423+manerger-manu@users.noreply.github.com>
2024-02-11 10:26:26 +0100

    Add files via upload

---
+++ KepFit/intranet.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KepFit</title>
+  <link rel="icon" type="image/x-icon" href="./assets/img/KepFit-Icon.png">
+  <!-- Agrega la referencia al archivo CSS de Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inclusive+Sans&family=Roboto:wght@100&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inclusive+Sans&family=Martian+Mono&family=Roboto:wght@100&display=swap">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+     <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="#index">
+          <img src="./assets/img/KepFit.png" alt="Logo" width="120" height="90">
+          KepFit - Intranet
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav d-flex">
+            <li class="nav-item ms-4">
+              <a class="nav-link active events tran" aria-current="page" href="logout.php">Salir</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+ <body>
+<div id="index" class="pagina">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="jumbotron">
+            <h1 class="display-3 text-center">KepFit</h1>
+            <p class="lead">Bienvenidos KepFit, somos una joven empresa con la idea de crear una sociedad mucho mas unida del mundo del fitnes mediante una red social con publicaciones, entrenadores, diferentes productos Y MIL POSIBILIDADES</p>
+            <hr class="my-4">
+            <p class="display-6">TRANSFORMA TU VIDA, COMPARTE TU ENERGIA.</p>
+           </div>
+            <img src="./assets/img/app.png" alt="coche" class="mx-auto d-block img-fluid">
+        </div>
+      </div>
+      <div class="row d-flex">
+        <div class="col-lg-6 col-md-6 col-sm-12 align-self-center">
+          <h3>Nuestro Proposito Es:</h3>
+          <p>
+          Aumentar la comunidad fitness e interconectarla aún más mediante rutinas, retos e interaciones en nuestra aplicación móvil.
+          Contando con entrenadores personales, personas con experiencia y ganas de entrenar.
+          </p>
+          <p>
+            Buscamos proveer a todos los usuarios de productos, de todo tipo, desde suplementación hasta maquinaria fitness.
+          </p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <img src="./assets/img/monigotes.png" alt="monigotes" class="mx-auto d-block img-fluid" height="400" width="400">
+        </div>
+      </div>
+      <div class="row d-flex justify-content-between bd-highlight">
+        <div class="col bd-highlight">
+          <img src="./assets/img/equipo.png" alt="equipo" class="mx-auto d-block img-fluid" height="400" width="400">
+        </div>
+        <div class="col bd-highlight align-self-center">
+          <h3>Contamos Con:</h3>
+          <p>
+            Grandes proveedores y consejeros para mejorar la experiencia del usuario. 
+            También tenemos en nuestro equipo grandes desarrolladores e ingenieros que nos ayudaran a conseguir una aplicación de gran calibre.
+          </p>
+        </div>
+        <!--<div class="col-lg-6 col-md-6 col-sm-12 align-self-center">
+        -->
+      </div>
+    </div>
+  </div>
+ </body>
+<footer class="footer bg-light text-lg-start ">
+  <div class="text-center">
+    <p>Rubén, Asier Lizarraga, Asier Mañeru, Jon y Enmanuel Holgado. 2ºASIR
+    </p>
+    <p class="mb-0">
+      Nuestras redes sociales:
+    </p>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <div class="collapse navbar-collapse d-flex justify-content-center align-self-center" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item ms-4">
+              <strong>Correo</strong> 
+              <a class="nav-link active" aria-current="page" href="#">
+                <img src="./assets/img/gmail.png" alt="gmail" class="img-fluid imgs">
+              </a>
+            </li>
+            <li class="nav-item ms-4">
+              <strong>Instagram</strong>
+              <a class="nav-link active" aria-current="page" href="#">
+              <img src="./assets/img/ig.png" alt="gmail" class="img-fluid imgs">
+              </a>
+            </li>
+            <li class="nav-item ms-4">
+            <strong>Infojobs</strong>
+              <a class="nav-link active" aria-current="page" href="#">
+                <img src="./assets/img/info.png" alt="gmail" class="img-fluid imgs">                
+              </a>
+            </li>
+            <li class="nav-item ms-4">
+              <strong>Linkdkelin</strong>
+              <a class="nav-link active"  aria-current="page" href="#">
+              <img src="./assets/img/link.png" alt="gmail" class="img-fluid imgs">
+            </a>
+            </li>
+            <li class="nav-item ms-4">
+              <strong>Facebook</strong>
+              <a class="nav-link active" aria-current="page" href="#">
+              <img src="./assets/img/face.png" alt="gmail" class="img-fluid imgs">
+            </a>
+            </li>
+            <li class="nav-item ms-4">
+              <strong>Twitter</strong>
+              <a class="nav-link active" aria-current="page" href="#">
+              <img src="./assets/img/twitter.png" alt="gmail" class="img-fluid imgs">
+            </a>
+            </li>
+          </ul>
+        </div>
+    </nav>
+  </div>
+</footer>
